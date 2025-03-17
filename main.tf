02d562183736b4c5c09b2b5d78168a8b8a8801fb
RupeshVanneldas <rvanneldas2000@gmail.com>
2025-03-16 22:58:46 -0400

    massive changes to ansible and tf code

--- main.tf
+++ main.tf
@@ -20,48 +20,49 @@ provider "azurerm" {
 # --------------------------------------------------------
 # Resource Group: Creates a new Azure resource group
 # --------------------------------------------------------
-resource "azurerm_resource_group" "test" {
-  name     = "test-resources-1"
+resource "azurerm_resource_group" "healthsync" {
+  name     = "healthsync-rg"
   location = "East US"
 }
 
 # --------------------------------------------------------
 # Virtual Network: Creates a VNet for the VM
 # --------------------------------------------------------
-resource "azurerm_virtual_network" "test" {
-  name                = "test-network"
+resource "azurerm_virtual_network" "healthsync" {
+  name                = "healthsync-network"
   address_space       = ["10.0.0.0/16"] # CIDR range for the VNet
-  location            = azurerm_resource_group.test.location
-  resource_group_name = azurerm_resource_group.test.name
+  location            = azurerm_resource_group.healthsync.location
+  resource_group_name = azurerm_resource_group.healthsync.name
 }
 
 # --------------------------------------------------------
 # Subnet: Creates a subnet within the VNet
 # --------------------------------------------------------
-resource "azurerm_subnet" "test" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.test.name
-  virtual_network_name = azurerm_virtual_network.test.name
-  address_prefixes     = ["10.0.2.0/24"] # CIDR range for the subnet
+resource "azurerm_subnet" "healthsync" {
+  name                 = "vm-subnet"
+  resource_group_name  = azurerm_resource_group.healthsync.name
+  virtual_network_name = azurerm_virtual_network.healthsync.name
+  address_prefixes     = ["10.0.1.0/24"] # CIDR range for the subnet
 }
 
 # --------------------------------------------------------
 # Public IP: Creates a public IP for the VM
 # --------------------------------------------------------
-resource "azurerm_public_ip" "test" {
-  name                = "test_pip"
-  resource_group_name = azurerm_resource_group.test.name
-  location            = azurerm_resource_group.test.location
+resource "azurerm_public_ip" "healthsync" {
+  name                = "healthsync_pip"
+  resource_group_name = azurerm_resource_group.healthsync.name
+  location            = azurerm_resource_group.healthsync.location
   allocation_method   = "Static" # IP will not change upon restart
+  domain_name_label   = "healthsync"
 }
 
 # --------------------------------------------------------
 # Network Security Group: Creates a new NSG to allow SSH and HTTP access
 # --------------------------------------------------------
-resource "azurerm_network_security_group" "test" {
-  name                = "test-nsg"
-  location            = azurerm_resource_group.test.location
-  resource_group_name = azurerm_resource_group.test.name
+resource "azurerm_network_security_group" "healthsync" {
+  name                = "healthsync-nsg"
+  location            = azurerm_resource_group.healthsync.location
+  resource_group_name = azurerm_resource_group.healthsync.name
 
   security_rule {
     name                       = "SSH"
@@ -76,19 +77,18 @@ resource "azurerm_network_security_group" "test" {
   }
 
   security_rule {
-    name                       = "Allow-Port-4000"
+    name                       = "Microservices-Ports"
     priority                   = 110
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "4000"
+    destination_port_range     = "4000-4010"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
-
   security_rule {
-    name                       = "Allow-Port-5000"
+    name                       = "DB-Port-5000"
     priority                   = 120
     direction                  = "Inbound"
     access                     = "Allow"
@@ -99,15 +99,15 @@ resource "azurerm_network_security_group" "test" {
     destination_address_prefix = "*"
   }
 
-  # Allow HTTP (port 8000) for React app
+  # Allow HTTP (port 80) for React app
   security_rule {
-    name                       = "Allow-Port-8000"
+    name                       = "Frontend-Port-80"
     priority                   = 130
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "8000"
+    destination_port_range     = "80"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
@@ -116,39 +116,39 @@ resource "azurerm_network_security_group" "test" {
 # --------------------------------------------------------
 # Network Interface: Associates the VM with the subnet and public IP
 # --------------------------------------------------------
-resource "azurerm_network_interface" "test" {
-  name                = "test-nic"
-  location            = azurerm_resource_group.test.location
-  resource_group_name = azurerm_resource_group.test.name
+resource "azurerm_network_interface" "healthsync" {
+  name                = "healthsync-nic"
+  location            = azurerm_resource_group.healthsync.location
+  resource_group_name = azurerm_resource_group.healthsync.name
   ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.test.id
+    name                          = "healthsync"
+    subnet_id                     = azurerm_subnet.healthsync.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.test.id
+    public_ip_address_id          = azurerm_public_ip.healthsync.id
   }
 }
 
 # --------------------------------------------------------
 # Associate the NSG with the Network Interface
 # --------------------------------------------------------
-resource "azurerm_network_interface_security_group_association" "test" {
-  network_interface_id      = azurerm_network_interface.test.id
-  network_security_group_id = azurerm_network_security_group.test.id
+resource "azurerm_network_interface_security_group_association" "healthsync" {
+  network_interface_id      = azurerm_network_interface.healthsync.id
+  network_security_group_id = azurerm_network_security_group.healthsync.id
 }
 
 # --------------------------------------------------------
 # Virtual Machine: Deploys an Ubuntu VM in Azure
 # --------------------------------------------------------
-resource "azurerm_linux_virtual_machine" "test" {
-  name                            = "test-machine"
-  resource_group_name             = azurerm_resource_group.test.name
-  location                        = azurerm_resource_group.test.location
-  size                            = "Standard_D2s_v3"
+resource "azurerm_linux_virtual_machine" "healthsync" {
+  name                            = "healthsync-machine"
+  resource_group_name             = azurerm_resource_group.healthsync.name
+  location                        = azurerm_resource_group.healthsync.location
+  size                            = "Standard_B2s"
   admin_username                  = "adminuser"
   disable_password_authentication = true # Disables password login for security
 
   network_interface_ids = [
-    azurerm_network_interface.test.id,
+    azurerm_network_interface.healthsync.id,
   ]
 
   # SSH Key Configuration
@@ -177,80 +177,29 @@ resource "azurerm_linux_virtual_machine" "test" {
   }
 }
 
-# # Grant ACR Pull Role to VM
-# resource "azurerm_role_definition" "acr_pull_custom" {
-#   name        = "AcrPullCustom"
-#   scope       = azurerm_container_registry.test.id
-#   description = "Custom role to pull images from ACR"
-
-#   permissions {
-#     actions = [
-#       "Microsoft.ContainerRegistry/registries/pull/read"
-#     ]
-#     not_actions = []
-#   }
-
-#   assignable_scopes = [
-#     azurerm_container_registry.test.id
-#   ]
-# }
-
-# resource "azurerm_role_assignment" "acr_pull" {
-#   scope              = azurerm_container_registry.test.id
-#   role_definition_id = azurerm_role_definition.acr_pull_custom.role_definition_resource_id
-#   principal_id       = azurerm_linux_virtual_machine.test.identity[0].principal_id
-
-#   lifecycle {
-#     prevent_destroy = true
-#   }
-
-#   depends_on = [azurerm_role_definition.acr_pull_custom]
-# }
-
 # Grant ACR Pull Role to VM
 resource "azurerm_role_assignment" "acr_pull" {
-  scope                = azurerm_container_registry.test.id
+  scope                = azurerm_container_registry.healthsync.id
   role_definition_name = "AcrPull"
-  principal_id         = azurerm_linux_virtual_machine.test.identity[0].principal_id
-
-  lifecycle {
-    prevent_destroy = true
-  }
-
-  depends_on = [azurerm_container_registry.test] # Ensures ACR is created first
+  principal_id         = azurerm_linux_virtual_machine.healthsync.identity[0].principal_id
+  depends_on           = [azurerm_container_registry.healthsync] # Ensures ACR is created first
 }
 
 # --------------------------------------------------------
 # Azure Container Registry (ACR): Creates a private container registry
 # --------------------------------------------------------
-resource "azurerm_container_registry" "test" {
-  name                = "rupeshcapstoneacr" # Must be globally unique
-  resource_group_name = azurerm_resource_group.test.name
-  location            = azurerm_resource_group.test.location
+resource "azurerm_container_registry" "healthsync" {
+  name                = "healthsyncacr" # Must be globally unique
+  resource_group_name = azurerm_resource_group.healthsync.name
+  location            = azurerm_resource_group.healthsync.location
   sku                 = "Basic" # Options: Basic, Standard, Premium
   admin_enabled       = true    # Enables admin login for easier access
 }
 
-# # --------------------------------------------------------
-# # Storage Account: Creates a new Azure Storage Account
-# # --------------------------------------------------------
-# resource "azurerm_storage_account" "test" {
-#   name                     = "rupeshcapstonesa" # Must be globally unique
-#   resource_group_name      = azurerm_resource_group.test.name
-#   location                 = azurerm_resource_group.test.location
-#   account_tier             = "Standard"
-#   account_replication_type = "LRS" # Locally redundant storage (LRS)
-
-#   tags = {
-#     environment = "test"
-#   }
-# }
-
-# # --------------------------------------------------------
-# # Blob Container: Creates a container inside the Storage Account for the tfstate file
-# # --------------------------------------------------------
-# resource "azurerm_storage_container" "tfstate" {
-#   name                  = "tfstate-container" # Container for tfstate files
-#   storage_account_name  = azurerm_storage_account.test.name
-#   container_access_type = "private" # Keeps it private for security reasons
-# }
+# --------------------------------------------------------
+# Output: Displays the public IP of the VM
+# --------------------------------------------------------
+output "public_ip" {
+  description = "Public IP address of the VM"
+  value       = azurerm_public_ip.healthsync.ip_address
+}
